f5dcaf9f1ad07d6c1b311d16a5a67f49fe63c155
Sathwik <sathwikdec@gmail.com>
2018-11-10 16:31:09 -0500

    moved from css to scss

---
+++ gulpfile.js
@@ -0,0 +1,19 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+
+var paths = {
+    sass: ['./scss/style.scss']
+};
+
+gulp.task('default', ['sass']);
+
+gulp.task('sass', function (done) {
+    gulp.src('./scss/style.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./css/'))
+        .on('end', done);
+})
+
+gulp.task('watch', ['sass'], function () {
+    gulp.watch(paths.sass, ['sass']);
+});
