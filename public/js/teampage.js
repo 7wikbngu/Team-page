13e660fb055505ec1624844797ecb29236ba2d3e
Sathwik <sathwikdec@gmail.com>
2018-12-07 13:56:42 -0500

    final changes to be made

--- public/js/teampage.js
+++ public/js/teampage.js
@@ -32,17 +32,14 @@ $(function () {
         console.error("Error: ", err);
     })
 
+
     function generateLeaders(leadersObj){
         filterLeader = leadersObj;
         console.log([leadersObj]);
-        if(filterStatus === 0){
-            for(var i = 0; i < leadersObj.length; i++){
-                var leaderHtml = '<div class="leader row mb-2 mx-3"><img src="https://placecage.com/125/125" class="rounded-circle col-5 align-self-center" alt=""><div class="leader-info text-uppercase align-self-center mt-1 col-7"><h5>'+leadersObj[i].fullname+'</h5><p>'+leadersObj[i].position+'</p></div></div>';
+        for(var i = 0; i < leadersObj.length; i++){
+            var leaderHtml = '<div class="leader row mb-4 mx-3"><img src="https://source.unsplash.com/random/125x125" class="rounded-circle col-5 align-self-center" alt=""><div class="leader-info text-capitalize align-self-center mt-1 col-7"><h5>'+leadersObj[i].fullname+'</h5><p>'+leadersObj[i].position+'</p></div></div>';
                 
-                $(".leadership").append(leaderHtml);
-            }
-        }else {
-
+            $(".leadership").append(leaderHtml);
         }
     }
 
@@ -53,7 +50,7 @@ $(function () {
         for (var i = 0; i < deptsObj.length; i++) {
             // console.log("Name: ",deptsObj[dept][i].fullname);
             // console.log("Position: ",deptsObj[dept][i].position);
-            var employeeHtml = '<div class="employee my-2 py-2"><img src="https://placecage.com/125/125" class="rounded-circle" alt=""><div class="employee-info invisible text-capitalize mt-1"><h5 id="employeeName">' + deptsObj[i].fname + ' ' + deptsObj[i].lname + '</h5><p>' + deptsObj[i].position + '</p></div></div>';
+            var employeeHtml = '<div class="employee my-2 py-2"><img src="https://source.unsplash.com/random/125x125" class="rounded-circle" alt=""><div class="employee-info invisible text-capitalize mt-1"><h5 id="employeeName">' + deptsObj[i].fname + ' ' + deptsObj[i].lname + '</h5><p>' + deptsObj[i].position + '</p></div></div>';
 
             $(".employees").append(employeeHtml);
         }
@@ -62,13 +59,13 @@ $(function () {
     // Search bar filter
     $('#filter').on('keyup', () => {
         var output = $("#filter").val();
-        var pattern = new RegExp(output.toLowerCase());
+        var pattern = new RegExp("\\b"+output.toLowerCase());
         console.log(output, employees);
         $(".employees").empty();
         employees.map( employee => {
-            if(pattern.test(employee.fname.toLowerCase()) || pattern.test(employee.lname.toLowerCase())){
+            if(pattern.test(employee.fname.toLowerCase()) || pattern.test(employee.lname.toLowerCase()) || pattern.test(employee.position.toLowerCase())){
                 console.log(employee.fname);
-                var employeeHtml = '<div class="employee my-2 py-2"><img src="https://placecage.com/125/125" class="rounded-circle" alt=""><div class="employee-info invisible text-capitalize mt-1"><h5 id="employeeName">' + employee.fname + ' ' + employee.lname + '</h5><p>' + employee.position + '</p></div></div>';
+                var employeeHtml = '<div class="employee my-2 py-2"><img src="https://source.unsplash.com/random/125x125" class="rounded-circle" alt=""><div class="employee-info invisible text-capitalize mt-1"><h5 id="employeeName">' + employee.fname + ' ' + employee.lname + '</h5><p>' + employee.position + '</p></div></div>';
                 $(".employees").append(employeeHtml);
             }
         });
